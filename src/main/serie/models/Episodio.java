f45ae1a2b3c7cd79bb5df468424feb3d80da4a9d
Carolina <carolinaldominguez@outlook.com>
2025-05-12 17:10:15 -0300

    adding exercise Serie

--- src/main/serie/models/Episodio.java
+++ src/main/serie/models/Episodio.java
@@ -1,5 +1,9 @@
 package main.serie.models;
 
 public class Episodio {
-    
+    private String titulo;
+    private String descripcion;
+    private boolean visto;
+    private int calificacion;
+    //valor negativo por defecto
 }
--- src/main/serie/models/Serie.java
+++ src/main/serie/models/Serie.java
@@ -1,5 +1,10 @@
 package main.serie.models;
 
 public class Serie {
-    
+    private String titulo;
+    private String descripcion;
+    private String creador;
+    private String genero;
+    //private Lis temporadas;
+    //buscar cual falta
 }
