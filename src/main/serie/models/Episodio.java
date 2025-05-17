1a32dfd6bb489cf41f40b57f2a850bc91e2e7054
Carolina <carolinaldominguez@outlook.com>
2025-05-17 15:23:43 -0300

    adding exercise Serie

--- src/main/serie/models/Episodio.java
+++ src/main/serie/models/Episodio.java
@@ -5,5 +5,18 @@ public class Episodio {
     private String descripcion;
     private boolean visto;
     private int calificacion;
-    //valor negativo por defecto
+
+    public Episodio(String nuevoTitulo,String nuevaDescripcion, boolean nuevaVista, int nuevaCalificacion ){
+    this.titulo=nuevoTitulo;
+    this.descripcion=nuevaDescripcion;
+    this.visto=nuevaVista;
+    this.calificacion=nuevaCalificacion;
+    }
+
+    public Episodio(String nuevoTitulo,String nuevaDescripcion, boolean nuevaVista){
+    this.titulo=nuevoTitulo;
+    this.descripcion=nuevaDescripcion;
+    this.visto=nuevaVista;
+    this.calificacion=(-1);
+    }
 }
