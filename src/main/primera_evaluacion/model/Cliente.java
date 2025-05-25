6fd42c9df7ece15c69ae7878494d17fe6268dd19
Carolina <carolinaldominguez@outlook.com>
2025-05-25 17:59:11 -0300

    Adding model Cliente

--- src/main/primera_evaluacion/model/Cliente.java
+++ src/main/primera_evaluacion/model/Cliente.java
@@ -1,5 +1,12 @@
 package main.primera_evaluacion.model;
 
-public class Cliente {
+public class Cliente extends Usuario{
+    public String nombreCompleto;
+    //Hereda de Usuario
+    /*
+    Hereda de Usuario. Agrega atributo nombreCompleto.
+     */
+    public Cliente(){
+    }
     
 }
