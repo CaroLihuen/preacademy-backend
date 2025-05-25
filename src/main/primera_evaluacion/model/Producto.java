ec236ef8e64c30233c90ee611ebe8f2d5946372c
Carolina <carolinaldominguez@outlook.com>
2025-05-25 17:57:15 -0300

    Adding model Producto

--- src/main/primera_evaluacion/model/Producto.java
+++ src/main/primera_evaluacion/model/Producto.java
@@ -1,5 +1,53 @@
 package main.primera_evaluacion.model;
 
+/*Atributos: int id, String nombre, double precio, String categoría.
+Implementar constructor vacío, getters y setters.
+Sobrescribir el método toString.
+Incluir patrón Builder dentro de la misma clase (lo que explicamos en la mentoría, como alternativa a los constructores).
+ */
 public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+    private String categoria;
+
+    public Producto(){
+
+    }
     
+    public Producto iD(int id){
+        this.id = id;
+        return this;
+    }
+
+    public Producto nombre(String nombre){
+        this.nombre = nombre;
+        return this;
+    }
+
+    public Producto precio(double precio){
+        this.precio = precio;
+        return this;
+    }
+
+    public double getPrecio(){
+        return precio; 
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public Producto categoria(String categoria){
+        this.categoria = categoria;
+        return this;
+    }
+    
+    @Override
+    public String toString(){
+        return " ID: " + this.id 
+        + ", Nombre: " + this.nombre 
+        + ", Precio: "+ this.precio 
+        + ", Categoría: "+ this.categoria;
+    }  
 }
