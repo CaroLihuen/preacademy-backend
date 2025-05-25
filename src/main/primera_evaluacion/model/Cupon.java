da858402342b41854324b2411475b328b5ab67a0
Carolina <carolinaldominguez@outlook.com>
2025-05-25 18:02:55 -0300

    Adding model Cupon

--- src/main/primera_evaluacion/model/Cupon.java
+++ src/main/primera_evaluacion/model/Cupon.java
@@ -1,5 +1,29 @@
 package main.primera_evaluacion.model;
 
 public class Cupon {
+    private String codigo;
+    private double porcentajeDescuento;
+
+    public Cupon(){
+    }
     
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public double getPorcentajeDesc(){
+        return porcentajeDescuento;
+    }
+
+    public void SetCodigo(String newCode){
+        codigo = newCode;
+    }
+
+    public void SetPorcentajeDesc(double newPorcDesc){
+        porcentajeDescuento = newPorcDesc;
+    }
+
+    /*Atributos: String codigo, double porcentajeDescuento.
+Implementar constructor vacío, getters y setters.
+ */
 }
