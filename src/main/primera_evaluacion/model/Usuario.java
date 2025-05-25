f7ba13994a037053796bb9611ecdf2236fe7cf0a
Carolina <carolinaldominguez@outlook.com>
2025-05-25 18:04:52 -0300

    Adding model Usuario

--- src/main/primera_evaluacion/model/Usuario.java
+++ src/main/primera_evaluacion/model/Usuario.java
@@ -1,5 +1,39 @@
 package main.primera_evaluacion.model;
 
 public class Usuario {
+    public Integer id;
+    public String nombreUsuario;
+    public String contraseña;
+    //utilizar wrappers
+    /*Atributos: id, nombreUsuario, contraseña (utilizar wrappers).*/ 
+ 
+    public Usuario(Integer newId, String newNombreU, String newContraseña){
+        this.id = newId;
+        this.nombreUsuario = newNombreU;
+        this.contraseña = newContraseña;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    public String getNombreU(){
+        return nombreUsuario; 
+    }
     
+    public String getContraseña(){
+        return contraseña;
+    }
+
+    public void SetId(Integer newId){
+        id = newId;
+    }
+
+    public void SetNombreU(String newNombreU){
+        nombreUsuario = newNombreU;
+    }
+
+    public void SetContraseña(String newContraseña){
+        contraseña = newContraseña;
+    }
 }
