a27e233875d3bd267915492905410faef2446bc5
Carolina <carolinaldominguez@outlook.com>
2025-08-03 16:18:08 -0300

    adding turno and profesional

--- src/main/segunda_evaluacion/src/main/java/sprint/segunda_evaluacion/model/Profesional.java
+++ src/main/segunda_evaluacion/src/main/java/sprint/segunda_evaluacion/model/Profesional.java
@@ -1,9 +1,31 @@
 package main.segunda_evaluacion.src.main.java.sprint.segunda_evaluacion.model;
 
+import main.segunda_evaluacion.src.main.java.sprint.segunda_evaluacion.utils.GeneradorID;
+
 public class Profesional {
     private Long id;
     private String nombreCompleto;
     private String especialidad;
 
-    public Profesional(){}
+    public Profesional(String nombreCompleto, String especialidad){
+        this.id = GeneradorID.generarProfesionalId();
+        this.nombreCompleto = nombreCompleto;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombreCompleto(){
+        return nombreCompleto;
+    }
+
+    public String getEspecialidad(){
+        return especialidad;
+    }
+
+    public void SetNombreCompleto(String nuevoNombreCompleto){
+        nombreCompleto = nuevoNombreCompleto;
+    }
+
+    public void SetEspecialidad(String nuevaEspecialidad){
+        especialidad = nuevaEspecialidad;
+    }
 }
--- src/main/segunda_evaluacion/src/main/java/sprint/segunda_evaluacion/model/Turno.java
+++ src/main/segunda_evaluacion/src/main/java/sprint/segunda_evaluacion/model/Turno.java
@@ -1,12 +1,45 @@
 package main.segunda_evaluacion.src.main.java.sprint.segunda_evaluacion.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import main.segunda_evaluacion.src.main.java.sprint.segunda_evaluacion.utils.GeneradorID;
 
 public class Turno {
     private Long id;
-    private Paciente paciente;
-    private Profesional profesional;
+    private List<Paciente> paciente = new ArrayList<>();
+    private List<Profesional> profesional = new ArrayList<>();
     private LocalDate fecha;
 
-    public Turno(){}
+    public Turno(List<Paciente> paciente, List<Profesional> profesional, LocalDate fecha){
+        this.id = GeneradorID.generarTurnoId();
+        this.paciente= paciente;
+        this.profesional= profesional;
+        this.fecha = fecha;
+    }
+
+    public List<Paciente> getPaciente(){
+        return paciente;
+    }
+
+    public List<Profesional> getProfesional(){
+        return profesional;
+    }
+
+    public LocalDate getFecha(){
+        return fecha;
+    }
+
+    public void SetPaciente(List<Paciente> nuevopaciente){
+       paciente = nuevopaciente;
+    }
+
+    public void SetProfesional(List<Profesional> nuevoprofesional){
+       profesional = nuevoprofesional;
+    }
+
+    public void SetFecha(LocalDate nuevafecha){
+       fecha = nuevafecha;
+    }
 }
